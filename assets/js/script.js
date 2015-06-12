5aee3d607a5a12f4a3a47bd59f5795ca4c3b2213
Lorena Skalac <lskalac@mathos.hr>
2015-06-12 05:10:48 +0200

    dodavanje edita

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,28 @@
 
 $(document).ready(function(){
 	
-	$(".btnAdd").click(function(){
+	$(document).on("click",".btnAdd",function(){
 		var inpt = $('input[name=checkListItem]').val();
-		$("ol").append('<li class ="task">' + inpt + '<button type ="submit" class ="btnDone">Done </button>' +'</li>');
+		$("ol").append('<li class ="task">' + inpt + '<button type ="submit" class ="btnDone">Done </button><button type"submit" class="btnEdit">Edit </button>' +'</li>');
 	});
 	   
-	$(document).on('click', '.btnDone', function (e) {
+	$(document).on("click", ".btnDone", function (e) {
 		e.preventDefault();
 		var x = $(this).parent().text();
-		var y = x.substring(1, (x.length) - 5);
+		var y = x.substring(0, (x.length) - 10);
 		$("ul").append('<li class="task">' + y + '</li>');
 		$(this).parent().remove();
 	});	    
-	  
+	
+    $(document).on('click', '.btnEdit', function(){
+       $(this).parent().attr('contenteditable','true');
+	   $(this).parent().append('<button class = "btnSave"> Save </button>');
+    });
+	
+	$(document).on('click', '.btnSave', function(){
+       $(this).parent().attr('contenteditable','false');
+		   $(this).remove();
+    });
+	
 });
 
